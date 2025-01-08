16a0408332e5e0267099a25e2d62eca96537360d
Kosta <kosta.ilev@gmail.com>
2025-01-08 23:10:34 +0200

    create initializeBoard function

--- VikingChess/VikingChess.cpp
+++ VikingChess/VikingChess.cpp
@@ -2,10 +2,62 @@
 //
 
 #include <iostream>
+using namespace std;
+
+const char EMPTY = '.', KING = 'K', DEFENDER_PIECE = 'D', ATTACKER_PIECE = 'A';
+int BOARD_SIZE = 11; 
+char board[11][11]; 
+
+void initializeBoard() 
+{
+    for (int i = 0; i < BOARD_SIZE; i++) 
+    {
+        for (int j = 0; j < BOARD_SIZE; j++) 
+        {
+            board[i][j] = EMPTY;
+        }
+    }
+    
+    board[BOARD_SIZE / 2][BOARD_SIZE / 2] = KING;
+
+    for (int i = 3; i < 8; i++) 
+    {
+        if (i == 5) continue;
+        board[5][i] = DEFENDER_PIECE;
+        board[i][5] = DEFENDER_PIECE;
+    }
+       
+    board[4][4] = DEFENDER_PIECE;
+    board[6][4] = DEFENDER_PIECE;
+    board[4][6] = DEFENDER_PIECE;
+    board[6][6] = DEFENDER_PIECE;
+
+    for (int i = 3; i <= 7; i++) 
+    {
+        
+        board[0][i] = ATTACKER_PIECE;
+        board[10][i] = ATTACKER_PIECE;
+        board[i][0] = ATTACKER_PIECE;
+        board[i][10] = ATTACKER_PIECE;
+    }
+
+    board[5][1] = ATTACKER_PIECE;
+    board[1][5] = ATTACKER_PIECE;
+    board[5][9] = ATTACKER_PIECE;
+    board[9][5] = ATTACKER_PIECE;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    initializeBoard();
+    for (int i = 0; i < 11; i++)
+    {
+        for (int j = 0; j < 11; j++)
+        {
+            cout << board[i][j];
+        }
+        cout << endl;
+    }
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
