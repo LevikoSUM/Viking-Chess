b894171708a3d3ae20616b02a217e73724feddaa
Kosta <kosta.ilev@gmail.com>
2025-01-14 23:25:12 +0200

    improve printBoard function

--- VikingChess/VikingChess.cpp
+++ VikingChess/VikingChess.cpp
@@ -4,129 +4,176 @@
 #include <iostream>
 using namespace std;
 
-const char EMPTY = '.', KING = 'K', DEFENDER_PIECE = 'D', ATTACKER_PIECE = 'A';
-int BOARD_SIZE = 11; 
-char board[11][11]; 
+const char EMPTY = ' ', KING = 'K', DEFENDER_PIECE = 'D', ATTACKER_PIECE = 'A';
+int BOARD_SIZE = 11;
+char board[11][11];
 
-void initializeBoard() 
+void initializeBoard()
 {
-    for (int i = 0; i < BOARD_SIZE; i++) 
-    {
-        for (int j = 0; j < BOARD_SIZE; j++) 
-        {
-            board[i][j] = EMPTY;
-        }
-    }
-    
-    board[BOARD_SIZE / 2][BOARD_SIZE / 2] = KING;
+	for (int i = 0; i < BOARD_SIZE; i++)
+	{
+		for (int j = 0; j < BOARD_SIZE; j++)
+		{
+			board[i][j] = EMPTY;
+		}
+	}
 
-    for (int i = 3; i < 8; i++) 
-    {
-        if (i == 5) continue;
-        board[5][i] = DEFENDER_PIECE;
-        board[i][5] = DEFENDER_PIECE;
-    }
-       
-    board[4][4] = DEFENDER_PIECE;
-    board[6][4] = DEFENDER_PIECE;
-    board[4][6] = DEFENDER_PIECE;
-    board[6][6] = DEFENDER_PIECE;
+	board[BOARD_SIZE / 2][BOARD_SIZE / 2] = KING;
 
-    for (int i = 3; i <= 7; i++) 
-    {
-        
-        board[0][i] = ATTACKER_PIECE;
-        board[10][i] = ATTACKER_PIECE;
-        board[i][0] = ATTACKER_PIECE;
-        board[i][10] = ATTACKER_PIECE;
-    }
+	for (int i = 3; i < 8; i++)
+	{
+		if (i == 5) continue;
+		board[5][i] = DEFENDER_PIECE;
+		board[i][5] = DEFENDER_PIECE;
+	}
 
-    board[5][1] = ATTACKER_PIECE;
-    board[1][5] = ATTACKER_PIECE;
-    board[5][9] = ATTACKER_PIECE;
-    board[9][5] = ATTACKER_PIECE;
+	board[4][4] = DEFENDER_PIECE;
+	board[6][4] = DEFENDER_PIECE;
+	board[4][6] = DEFENDER_PIECE;
+	board[6][6] = DEFENDER_PIECE;
+
+	for (int i = 3; i <= 7; i++)
+	{
+
+		board[0][i] = ATTACKER_PIECE;
+		board[10][i] = ATTACKER_PIECE;
+		board[i][0] = ATTACKER_PIECE;
+		board[i][10] = ATTACKER_PIECE;
+	}
+
+	board[5][1] = ATTACKER_PIECE;
+	board[1][5] = ATTACKER_PIECE;
+	board[5][9] = ATTACKER_PIECE;
+	board[9][5] = ATTACKER_PIECE;
 }
 
-void printBoard() 
+void printBoard()
 {
-    cout << "   ";
-    for (int i = 0; i < BOARD_SIZE; i++) 
-    {
-        char cordinate = 'A' + i;
-        cout << cordinate << " ";
-    }
-    cout << endl;
-    for (int i = 0; i < BOARD_SIZE; i++) 
-    {
-        cout << i + 1 << " ";
-        if (i + 1< 10)
-        {
-            cout << " ";
-        }
-        for (int j = 0; j < BOARD_SIZE; j++) 
-        {
-            cout << board[i][j] << " ";
-        }
-        cout << endl;
-    }
+	for (int i = 0; i < BOARD_SIZE; ++i)
+	{
+		cout << "    ";
+		for (int j = 0; j < BOARD_SIZE; ++j)
+		{
+			cout << "+-------";
+		}
+		cout << "+" << endl;
+
+		for (int rowPart = 0; rowPart < 3; ++rowPart)
+		{
+			if (rowPart == 1)
+			{
+
+				if (i + 1 < 10)
+				{
+					cout << "   " << i + 1;
+				}
+				else
+				{
+					cout << "  " << i + 1;
+				}
+			}
+			else
+			{
+				cout << "    ";
+			}
+
+			for (int j = 0; j < BOARD_SIZE; ++j)
+			{
+				if (rowPart == 1 && board[i][j] != ' ')
+				{
+
+					cout << "|   " << board[i][j] << "   ";
+				}
+				else
+				{
+
+					cout << "|       ";
+				}
+			}
+			cout << "|" << endl;
+		}
+	}
+
+
+	cout << "    ";
+	for (int j = 0; j < BOARD_SIZE; ++j)
+	{
+		cout << "+-------";
+	}
+	cout << "+" << endl;
+
+
+	cout << "    ";
+	for (int j = 0; j < BOARD_SIZE; ++j)
+	{
+		if (j + 1 < 10)
+		{
+			cout << "    " << j + 1 << "   ";
+		}
+		else
+		{
+			cout << "   " << j + 1 << "   ";
+		}
+	}
+	cout << endl;
 }
 
-bool isValidMove(int startX, int startY, int endX, int endY) 
+bool isValidMove(int startX, int startY, int endX, int endY)
 {
-    if (endX < 0 || endX >= BOARD_SIZE || endY < 0 || endY >= BOARD_SIZE) return false; // Out of bounds
-    if (board[endX][endY] != EMPTY) return false; // Destination not empty
-    if (board[startX][startY] == EMPTY) return false; // Not a piece
-    if (startX != endX && startY != endY) return false; // Only straight moves allowed
+	if (endX < 0 || endX >= BOARD_SIZE || endY < 0 || endY >= BOARD_SIZE) return false; // Out of bounds
+	if (board[endX][endY] != EMPTY) return false; // Destination not empty
+	if (board[startX][startY] == EMPTY) return false; // Not a piece
+	if (startX != endX && startY != endY) return false; // Only straight moves allowed
 
-    int directionX = (endX > startX) - (endX < startX);
-    int directionY = (endY > startY) - (endY < startY);
+	int directionX = (endX > startX) - (endX < startX);
+	int directionY = (endY > startY) - (endY < startY);
 
-    int currentX = startX + directionX;
-    int currentY = startY + directionY;
-    while (currentX != endX || currentY != endY) 
-    {
-        if (board[currentX][currentY] != EMPTY) return false; // Cannot jump over other pieces
-        currentX += directionX;
-        currentY += directionY;
-    }
+	int currentX = startX + directionX;
+	int currentY = startY + directionY;
+	while (currentX != endX || currentY != endY)
+	{
+		if (board[currentX][currentY] != EMPTY) return false; // Cannot jump over other pieces
+		currentX += directionX;
+		currentY += directionY;
+	}
 
-    return true;
+	return true;
 }
 
-void makeMove(int startX, int startY, int endX, int endY) 
+void makeMove(int startX, int startY, int endX, int endY)
 {
-    if (isValidMove(startX, startY, endX, endY)) 
-    {
-        char piece = board[startX][startY];
-        board[startX][startY] = EMPTY;
-        board[endX][endY] = piece;
-        cout << "Moved piece from (" << startX + 1 << ", " << startY + 1 << ") to (" << endX + 1 << ", " << endY + 1 << ")." << endl;
-    }
-    else 
-    {
-        cout << "Invalid move. Try again." << endl;
-    }
+	if (isValidMove(startX, startY, endX, endY))
+	{
+		char piece = board[startX][startY];
+		board[startX][startY] = EMPTY;
+		board[endX][endY] = piece;
+		cout << "Moved piece from (" << startX + 1 << ", " << startY + 1 << ") to (" << endX + 1 << ", " << endY + 1 << ")." << endl;
+	}
+	else
+	{
+		cout << "Invalid move. Try again." << endl;
+	}
 }
 
 int main()
 {
-    initializeBoard();
-    printBoard();
+	initializeBoard();
+	printBoard();
 
-    char command[10];
-    while (true) 
-    {
-        cout << "> ";
-        cin >> command;
+	char command[10];
+	while (true)
+	{
+		cout << "> ";
+		cin >> command;
 
-        if (command[0] == 'm' && command[1] == 'o' && command[2] == 'v' && command[3] == 'e')
-        {
-            int startX, startY, endX, endY;
-            cin >> startX >> startY >> endX >> endY;
-            makeMove(startX - 1, startY - 1, endX - 1, endY - 1);
-            printBoard();
-        }
-    }
+		if (command[0] == 'm' && command[1] == 'o' && command[2] == 'v' && command[3] == 'e')
+		{
+			int startX, startY, endX, endY;
+			cin >> startX >> startY >> endX >> endY;
+			makeMove(startX - 1, startY - 1, endX - 1, endY - 1);
+			printBoard();
+		}
+	}
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
